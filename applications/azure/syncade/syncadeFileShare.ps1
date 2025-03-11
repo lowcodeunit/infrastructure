62cb94d2794ddce0ff965175879a65580f1178ef
Skeletor11 <kevinbowersdeveloper@gmail.com>
2025-03-10 20:01:34 -0600

    permissions

--- applications/azure/syncade/syncadeFileShare.ps1
+++ applications/azure/syncade/syncadeFileShare.ps1
@@ -59,7 +59,7 @@ try {
  
     # Step 6: Map the Z drive
     Write-Output "Mapping the Z drive..." | Out-File -Append -FilePath $logFile
-    net use Z: \\$storageAccountName.file.core.windows.net\sharedfiles /user:Azure\$storageAccountName $storageKey /persistent:yes
+    net use Z: \\$storageAccountName.file.core.windows.net\sharedfiles /user:Azure\$storageAccountName $storageKey /savecred /persistent:yes
  
     Start-Sleep -Seconds 2
  
@@ -73,7 +73,7 @@ try {
  
     # Step 8: Create Scheduled Task for Persistence
     Write-Output "Creating scheduled task..." | Out-File -Append -FilePath $logFile
-    schtasks /create /tn "MapZDrive" /tr "cmd /c net use Z: \\$storageAccountName.file.core.windows.net\sharedfiles /user:Azure\$storageAccountName $storageKey /persistent:yes" /sc onlogon /rl highest /f
+    schtasks /create /tn "MapZDrive" /tr "cmd /c net use Z: \\$storageAccountName.file.core.windows.net\sharedfiles /user:Azure\$storageAccountName $storageKey /savecred /persistent:yes" /sc onlogon /ru "SYSTEM" /rl highest /f
  
     Write-Output "Z drive mapping and scheduled task setup complete." | Out-File -Append -FilePath $logFile
     exit 0
